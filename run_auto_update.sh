37830a6da2acb5a803d606357fb5de7997ec5685
jim <27871987+jimf256@users.noreply.github.com>
2023-05-20 19:01:21 +0100

    fixed copy to tmp directory not working

--- run_auto_update.sh
+++ run_auto_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 run_dir="/tmp/auto-update"
+script_dir="$(dirname $0)"
 
 # clean up and recreate temp directory
 if [ -d "$run_dir" ]; then
@@ -9,8 +10,8 @@ fi
 mkdir -p "$run_dir"
 
 # copy python file and environment to temp directory
-cp "auto-update.py" "$run_dir/"
-cp -r "env" "$run_dir/"
+cp "$script_dir/auto-update.py" "$run_dir/"
+cp -r "$script_dir/env" "$run_dir/"
 
 # run the python script
 $run_dir/env/bin/python $run_dir/auto-update.py -quiet
